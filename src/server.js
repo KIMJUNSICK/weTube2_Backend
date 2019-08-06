bc79ca3a8d5c02f44e7dff369e7de55266e91703
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-06 09:07:12 +0900

    basic Sever

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log("go!");
+require("dotenv").config();
+import express from "express";
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`Listening on: ✅  http://localhost:${PORT}`);
+
+app.get("/", (req, res) => {
+  res.send("junsik");
+});
+
+app.listen(PORT, handleListening);
