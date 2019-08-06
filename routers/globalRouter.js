5a6b6ec2026790d9f6efb42a34ebae16bd1178cc
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-06 20:42:26 +0900

    Revise Controllers

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import routes from "../routes";
 import { join, login, logout } from "../controllers/userController";
-import { home } from "../controllers/videoControllers";
+import { home, search } from "../controllers/videoControllers";
 
 const globalRouter = express.Router();
 
@@ -9,6 +9,6 @@ globalRouter.get(routes.home, home);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.join, join);
-globalRouter.get(routes.home, join);
+globalRouter.get(routes.search, search);
 
 export default globalRouter;
