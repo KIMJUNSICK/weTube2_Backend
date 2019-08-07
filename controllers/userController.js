c85f0bbaca618c8d7fbb3ed87da9d4c2c432b33c
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-07 13:28:20 +0900

    Template Variables in Pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
-export const join = (req, res) => res.render("join");
-export const logIn = (req, res) => res.render("logIn");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const logIn = (req, res) => res.render("logIn", { pageTitle: "Log In" });
 export const logOut = (req, res) => res.render("logOut");
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
