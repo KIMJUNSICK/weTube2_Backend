4b1c1671b7f239a4328f249bbe641d4405713f66
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-07 13:18:08 +0900

    Local Variables in Pug

--- src/app.js
+++ src/app.js
@@ -8,15 +8,17 @@ import userRouter from "../routers/userRouter";
 import globalRouter from "../routers/globalRouter";
 import videoRouter from "../routers/videoRouter";
 import routes from "../routes";
+import { localMiddleware } from "../middlewares";
 
 const app = express();
 
+app.use(Helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(Helmet());
 app.use(logger("dev"));
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
