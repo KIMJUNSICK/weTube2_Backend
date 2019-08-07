f5f53c2c10c34d990133846ec8d5b259d00d166f
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-07 21:24:46 +0900

    search Controller

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,6 +1,10 @@
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", potato: 1234 });
-export const search = (req, res) => res.render("search");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
 export const upload = (req, res) => res.render("upload");
 export const videoDetail = (req, res) => res.render("videoDetail");
 export const editVideo = (req, res) => res.render("editVideo");
