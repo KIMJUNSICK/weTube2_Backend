7ea66a2660f60704b15faeb5ba81a2af23f6fe48
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-08 15:46:18 +0900

    Join Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,19 @@
+import routes from "../routes";
+
 export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    res.redirect(routes.home);
+    console.log(req);
+  }
+};
+
 export const logIn = (req, res) => res.render("logIn", { pageTitle: "Log In" });
 export const logOut = (req, res) => res.render("logOut");
 export const userDetail = (req, res) => res.render("userDetail");
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,16 @@
 import express from "express";
 import routes from "../routes";
-import { join, logIn, logOut } from "../controllers/userController";
+import { join, logIn, logOut, postJoin } from "../controllers/userController";
 import { home, search } from "../controllers/videoControllers";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, join);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.logIn, logIn);
 globalRouter.get(routes.logOut, logOut);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.search, search);
 
 export default globalRouter;
